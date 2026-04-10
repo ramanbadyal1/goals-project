79efa81a726bfe716fa7c71f49ecddbba0e44e64
ramanbadyal <ramanbadyal@gmail.com>
2026-04-10 13:34:09 -0400

    last working version of the app.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
       setIsLoading(true);
 
       try {
-        const response = await fetch('/api/goals/');
+        const response = await fetch('http://localhost:5001/goals'); // subjected to change
 
         const resData = await response.json();
 
@@ -39,7 +39,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('/api/goals/', {
+      const response = await fetch('http://localhost:5001/goals', { // subjected to change
         method: 'POST',
         body: JSON.stringify({
           text: goalText,
@@ -78,7 +78,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('/api/goals/' + goalId, {
+      const response = await fetch('http://localhost:5001/goals' + goalId, { // subjected to change
         method: 'DELETE',
       });
 
